faf162faa30276f31267f65c5d7154d75a82caa5
Regan Layman <regan.layman@gmail.com>
2020-08-28 19:29:30 -0400

    added new get route for banana and carrot, so changing url /carrot /banana etc

--- sentenceOnBrowser/produce.rb
+++ sentenceOnBrowser/produce.rb
@@ -1,5 +1,13 @@
 require "sinatra"
 
 get("/apple") do
-  "Here's a juicy apple!"
+  "<h1>Here's a juicy apple!</h1>"
+end
+
+get("/banana") do
+  "<h1>Here's a ripe banana!</h1>"
+end
+
+get("/carrot") do
+  "<h1>Here's a crunchy carrot!</h1>"
 end
