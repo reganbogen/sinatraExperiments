a3c078f2b2d1a9b636d4d4d55420d34e1350313f
Regan Layman <regan.layman@gmail.com>
2020-08-29 13:41:18 -0400

    added a pages folder with a text file for plain ruby method page_content. called toggles information with puts page_content(toggle) and the method adds the .txt file name

--- wiki/test.rb
+++ wiki/test.rb
@@ -1,12 +1,7 @@
-require "sinatra"
-
 def page_content(title)
   File.read("pages/#{title}.txt")
 rescue Errno::ENOENT
   return nil
 end
 
-get "/" do
-  erb :welcome
-end
-
+puts page_content("toggle")
